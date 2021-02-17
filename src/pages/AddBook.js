74512f702dfb9a8cb7ca637622f4cf2f36c1476f
Kat Rohrmeier <kat.rohrmeier@propellerhealth.com>
2021-02-16 20:03:09 -0600

    [Take-1] add url book search when adding title; alphabetize json object

--- src/pages/AddBook.js
+++ src/pages/AddBook.js
@@ -3,31 +3,52 @@ import { Col, Container, Row, Button } from "react-bootstrap";
 import Form from 'react-bootstrap/Form';
 
 import books from '../info/books.json';
-import './add.css';
+import './book.css';
 
 export function AddBook() {
 
-  // use an onBlur event or make the enter title / ISBN be "look-up by title / ISBN" fields 
+  // TODO: use an onBlur event or make the 
+  // enter title / ISBN be "look-up by title / ISBN" fields 
   // that you then submit when you found the title you want
   // the onBlur or look-up fields connect to api
 
   const [title, setTitle] = useState('');
   const [validated, setValidated] = useState(false);
+  const [returnedTitle, setReturnedTitle] = useState('');
 
-  function createBookObject() {
+  const googleAPI = "https://www.googleapis.com/books/v1/volumes";
+
+  function findBook() {
+    const queryParams = encodeURIComponent(title);
+    const searchURL = `${googleAPI}?q=${queryParams}`;
+    console.log('api url = ', searchURL);
+    fetch(searchURL)
+        .then(response => response.json())
+        .then(data => {
+          const authors = data.items[0].volumeInfo.authors;
+          const apiGenre = data.items[0].volumeInfo.categories;
+          const apiIsbn = data.items[0].volumeInfo.industryIdentifiers[0].identifier;
+          const apiPageCount = data.items[0].volumeInfo.pageCount;
+          const apiTitle = data.items[0].volumeInfo.title;
+          setReturnedTitle(apiTitle);
+          addNewBook(createBookObject(authors, apiGenre, apiIsbn, apiPageCount, apiTitle));
+        })
+        .catch(error => console.error(`error : ${error}`));
+  }
+
+  function createBookObject( authors, genre, isbn, pageCount, title ) {
     const today = new Date();
     const formattedDate = today.toISOString().slice(0,10);
     return (
       {
-        "isbn": "0",
-        "title": title,
-        "author": "",
-        "pages": "0",
-        "onLoan": "false",
-        "favorite": "false",
+        "authors": authors,
         "dateAdded": formattedDate,
-        "genre": 
-          []
+        "favorite": "false",
+        "genre": genre,
+        "isbn": isbn,
+        "onLoan": "false",
+        "pageCount": pageCount,
+        "title": title
       }
     )
   }
@@ -44,16 +65,13 @@ export function AddBook() {
     }
   }
 
-  function addNewBook() {
-    const newBook = createBookObject();
+  function addNewBook(newBook) {
     books.push(newBook);
-    console.log('added new book ** ** books: ', books);
+    console.log('books w/pushed new book = ', books);
     setTitle('');
-    console.log('book title should now be blank: ', title);
   }
 
   function handleChange(e) {
-    console.log('handle change, e.target.value = ', e.target.value);
     setTitle(e.target.value);
   }
 
@@ -66,7 +84,7 @@ export function AddBook() {
   function handleSubmit(e) {
     e.preventDefault();
     if (validateInput()) {
-      addNewBook();
+      findBook();
     }
   }
 
@@ -103,8 +121,9 @@ export function AddBook() {
             <Button type='submit'>Enter new book</Button>
           </Form>
           <p className={validated ? 'success-message' : 'hidden'}>
-            {`Your new book's been added! You now have ${books.length} titles in your library.`}
+            {`Added "${returnedTitle}"! You now have ${books.length} titles in your library.`}
           </p>
+          <p id='content'></p>
     </Container>
   )
 }
