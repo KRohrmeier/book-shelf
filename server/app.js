b8291a96138716a94e1d272a4f96dce8a09cd9cc
Kat Rohrmeier <kat.rohrmeier@propellerhealth.com>
2021-08-01 18:31:16 -0500

    [mongo-kat] guess we need to add the deprecated body-parser to parse form data

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var app = express();
 var port = 3000;
 
-const router = express.Router();
-// app.get("/", (req, res) => {
-//  res.send("Hello Unicorn Army");
-// });
+app.use(bodyParser.urlencoded({ extended: true }));
 
- 
 app.listen(port, () => {
  console.log("Server listening on port " + port);
 });
@@ -17,7 +14,7 @@ app.listen(port, () => {
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://<userName>:<password>0.xy67p.mongodb.net/bookshelf_dev");
+mongoose.connect("mongodb+srv://{userName}:{password}@cluster0.xy67p.mongodb.net/bookshelf_dev", { useNewUrlParser: true, useUnifiedTopology: true });
 
 var bookkeeperSchema = new mongoose.Schema({
     givenName: String,
@@ -33,66 +30,20 @@ app.get("/", (req, res) => {
 });
 
 app.post("/addBookkeeper", (req, res) => {
-  console.log('* * * HERE! req = ', req);
+  console.log('* * * in addBookkeeper; req.body) = ', req.body);
   
-  var newBookkeeper = new Bookkeeper({ givenName: "hereIsAName", email: "hereIsAnEmail", phone: "hereIsAPhone" });
+  var newBookkeeper = new Bookkeeper({ 
+    givenName: req.body.givenName,
+    email: req.body.email,
+    phone: req.body.phone
+  });
+  console.log('newBookkeeper = ', newBookkeeper);
   // var newBookkeeper = new Bookkeeper(req.body);
   newBookkeeper.save()
-    .then(book => {
+    .then(bookkeeper => {
       res.send("bookkeeper saved to database");
     })
     .catch(err => {
-      res.status(400).send("WHOOPS");
+      res.status(400).send("WHOOPS! Unable to save bookkeeper to database :( ");
     })
 });
-
-// export const sendEmailAddress = (data) => {
-//   return fetch('/api/braze/contact', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(data),
-//   }).then((res) => res.json());
-// };
-
-// const sendToBraze = attributes => {
-//   const body = createAliasBody(attributes);
-//   const options = {
-//     method: 'POST',
-//     uri: BRAZE_URL,
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     body: body,
-//     json: true
-//   };
-//   return rp(options);
-// };
-
-// router.post('/contact', (req, res) => {
-//   if (
-//     !req.body.disease ||
-//     !req.body.email ||
-//     !req.body.group ||
-//     !req.body.reason
-//   ) {
-//     return res.status(400).json({
-//       error: `required information is missing`
-//     });
-//   }
-
-//   sendToBraze(req.body)
-//     .then(brazeRes => res.json(brazeRes))
-//     .catch(err => {
-//       res.status(400).json(err);
-//     });
-// });
-
-// const fieldValues = this.getAllValues(); <- object = {disease: 'asthma', email: 'email.com'}
-//     sendEmailAddress(fieldValues)
-//       .then((response) => {
-//         if (response.message === 'success') {
-//           afterSubmit('success');
-//         }
-//       );
