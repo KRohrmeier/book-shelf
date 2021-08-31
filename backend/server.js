1ad4b84e6fa44ecfdbf6f36ce87f622b63b9c1ea
Kat Rohrmeier <kat.rohrmeier@propellerhealth.com>
2021-08-31 18:12:31 -0500

    emoji branch - not working as-is, but emojis & styling are in here

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
+import books from "../src/api/books.route";
+import bookkeepers from "../src/api/bookkeepers.route";
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
 // configure using the environment variables from the .env file
 require('dotenv').config();
@@ -14,16 +16,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //possibly don't need this?
 
-const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log('MongoDB database connection established :) ');
-});
+// Not sure about using mongoose, tbh...
+// const uri = process.env.ATLAS_URI;
+// mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+// const connection = mongoose.connection;
+// connection.once('open', () => {
+//   console.log('MongoDB database connection established :) ');
+// });
+
+// Register api routes
+app.use("/api/v1/books", books);
+app.use("/api/v1/bookkeepers", bookkeepers)
+// app.use("/status", express.static("build"))
+// app.use("/", express.static("build"))
+// app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
 
 // start the server
 app.listen(port, () => {
   console.log('Server is running on port: ', port);
 });
 
-// installed nodemon, so type `nodemon server` in terminal to start
+export default app;
