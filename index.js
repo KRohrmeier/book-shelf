e7f4b8028cce1da7250e316462bd6e1884e6a0c4
Kat Rohrmeier <kat.rohrmeier@propellerhealth.com>
2021-08-31 13:22:55 -0500

    [fresh-MERN] ran `npm install dotenv`, added .env
    - added .env.example
    - added url I'm following for this MERN setup

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+console.log('index.js process.env.MONGODB_URI = ', process.env.MONGODB_URI);
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/my-book-shelf`);
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('my books(h)elf is running on port ${PORT}');
+});
